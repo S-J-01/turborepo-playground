3f7596eab983209b4d0a3e876dc5a644d3976161
Shreyas Jaltare <shreyasjaltare@gmail.com>
2025-06-18 21:39:56 +0530

    added one div in branch-one

--- apps/docs/app/page.tsx
+++ apps/docs/app/page.tsx
@@ -3,5 +3,15 @@ import { Button } from "@repo/ui/button";
 import styles from "./page.module.css";
 
 export default function Home() {
-  return <div>Changes made using SSH</div>;
+  return (
+    <div>
+      Changes made using SSH
+      <div>
+        Extra div added in branch one. Lorem ipsum dolor, sit amet consectetur
+        adipisicing elit. Illum architecto possimus ipsa dolor minima excepturi,
+        exercitationem esse fugiat maxime voluptatibus dolorum enim magni
+        mollitia, error totam eligendi vitae ab quam.
+      </div>
+    </div>
+  );
 }
